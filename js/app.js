883f1dad8fb226d381503915c46e85c2f5cf7f3a
Michael Q <MichaelQuiban@outlook.com>
2016-10-17 22:27:36 -0700

    Added Function
    Created a function called whoWon which will check if either of the users
    have filled a certain amount of squares (horizontally currently) has
    won.
    
    We will create another piece in the function which does the same thing..
    only diagnol.

--- js/app.js
+++ js/app.js
@@ -16,11 +16,27 @@ $(document).ready(function(){
 			// Bounce back and forth between the two players.
 			if (player === 1){
 				squareSelected.addClass('ex');
+
 				player = 2;
 			} else {
 				squareSelected.addClass('oh');
+
 				player = 1;
 			}
 		}
 	});
+
+	//Check if the player won by checking squares!
+	function whoWon(symbol) {
+		if($(".sq1").hasClass(symbol) && $(".sq2").hasClass(symbol) && $(".sq3").hasClass(symbol) {
+			return true;
+		} else if($(".sq4").hasClass(symbol) && $(".sq5").hasClass(symbol) && $(".sq6").hassClass(symbol)) {
+			return true;
+		} else if($(".sq7").hasClass(symbol) && $(".sq8").hasClass(symbol) && $(".sq9").hassClass(symbol)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 });
