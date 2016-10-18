a4da732b103ab457ac13b23fa74871728a5b3961
Michael Q <MichaelQuiban@outlook.com>
2016-10-17 22:15:39 -0700

    Added IF Statement
    I've added the ability for both users to bounce back and forth based on
    their classes.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,26 @@
 $(document).ready(function(){
+	var player = 1;
+
 	// Event Handler for click events on each of the square.
 	$(".square").on('click', function (event) {
-		// Debug Text to verify the squares work
-		console.log("A Square has been chosen!");
-		// Created 
+		// Debug text to verify the squares work
+		console.log("A square has been chosen!");
+		// Grab each square that is clicked.
 		var squareSelected = $(this);
+		//Debug text to display what 'this' is.
 		console.log(this);
-		//
+		// When the square has a class of 'ex' or 'oh' display a message.
 		if (squareSelected.hasClass('ex') || squareSelected.hasClass('oh')) {
-			alert("This square has already been selected! Please select another");
+			alert("This square has already been selected! Please select another.");
 		} else {
-
+			// Bounce back and forth between the two players.
+			if (player === 1){
+				squareSelected.addClass('ex');
+				player = 2;
+			} else {
+				squareSelected.addClass('oh');
+				player = 1;
+			}
 		}
 	});
 });
