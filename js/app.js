a69a63680dbb330ffbf7db5af7e27473f9dd068d
Michael Q <MichaelQuiban@outlook.com>
2016-10-18 21:11:24 -0700

    Added Reset button, replaced alerts with placeholder.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
-$(document).ready(function(){
+// Write code on a white board
+//Disadvantage to computer: Syntax highlighting, so much aids, not really thinking it through. Practice with hand!
+//Data Structure: Stacks, and queues, push, pop, NQ, DQ. First in first out, last in first out. Linked lists.
+//Node, knows itself and what comes next. How many items in this linked list, go to this node. <--- Study
+//Line of people holding a code to the guy infront of him, standing at the end, how many guys in that line?
+//Add an item from a list, delete an item from the list, middle of the row, pull a guy out with the red or blue coat.
+//Double linked list: Whats infront and whats behind you, go both ways, forward and backwards.
+//Trees: Where you say, you have a node, and a node has children, a binary tree has two children, left right, it can also
+//go multiple levels deep.
+//Special class of a tree: Binary Search Tree <--- We'll work on this. Series of numbers, children the one on the left
+//less than or greater than a number.
+//Google Balanced binary search tree. ( Graphs, connected by links ) <--- look up, facebook uses?
+
+$(document).ready(function() {
 	var player = 1;
 
 	// Event Handler for click events on each of the square.
@@ -9,23 +22,26 @@ $(document).ready(function(){
 		var squareSelected = $(this);
 		//Debug text to display what 'this' is.
 		console.log(this);
-		// When the square has a class of 'ex' or 'oh' display a message.
-		if (squareSelected.hasClass('fa fa-heart') || squareSelected.hasClass('fa fa-circle-thin')) {
-			alert("This square has already been selected! Please select another.");
+		// When the square has a class of 'fa-heart' or 'fa-circle' display a message.
+		if (squareSelected.hasClass('fa fa-heart') || squareSelected.hasClass('fa fa-circle-thin')) {		
 		} else {
-			// Bounce back and forth between the two players.
-			if (player === 1){
+			// Bounce back and forth between the two players and decide a winner.
+			if (player === 1) {
 				squareSelected.addClass("fa fa-heart");
+				console.log("heart added");
 				if(whowondisgame("fa fa-heart")) {
-					alert("Congrats! Player " + player + " has won!");
+					// Replace with appended results somewhere
+					//alert("Congrats! Player " + player + " has won!");
+					$(".winner").append("good job");
 				} else {
 				  player = 2;
 				}
 			} else {
 				squareSelected.addClass("fa fa-circle-thin");
 				if(whowondisgame("fa fa-circle-thin")) {
-					alert("Congrats! Player " + player + " has won!");
-				} else{
+					// Replace with appended results somewhere
+					alert("Congrats! Player " + player + " has won!");	
+				} else {
 					player = 1;
 				}
 			}
@@ -53,21 +69,12 @@ $(document).ready(function(){
 			return true;
 		} else {
 			return false;
-		} 
-		 
+		} 	 
 	}
 	//Reset the game
 	$(".reset-game").on('click', function (event) {
 		event.preventDefault();
-		$(".sq1").val("");
-		$(".sq2").val("");
-		$(".sq3").val("");
-		$(".sq4").val("");
-		$(".sq5").val("");
-		$(".sq6").val("");
-		$(".sq7").val("");
-		$(".sq8").val("");
-		$(".sq9").val("");
+		$(".square").removeClass("fa fa-heart fa-circle-thin ");
 	});
 
 });
